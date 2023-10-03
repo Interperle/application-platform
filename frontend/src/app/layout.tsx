6d8d59543cbd3be16987e83420319b43f409aacd
Marib Aldoais <marib.aldoais@gmail.com>
2023-10-03 10:03:06 +0200

    Created Header

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Apl_Header from '@/components/header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex flex-col min-h-screen">
+          <Apl_Header />
+          <div className="flex-grow">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   )
 }
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Apl_Header: React.FC = () => {
+  return (
+    <div className="w-full bg-white h-24 flex items-center justify-between p-4 md:p-6">
+        <div className="min-w-[20px] max-w-[200px]">
+            <Link href="https://generation-d.org" target="_blank">
+                <img src="/logos/gend_img_font.png" alt="Generation-D Logo" className="max-w-full h-auto" />
+            </Link>
+        </div>
+
+        <div className="flex space-x-4">
+            <button className="text-[#153757] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-#537575">
+            FAQs
+            </button>
+            <button className="border rounded px-4 py-2 text-[#FDCC89] bg-[#153757]">
+                Ausloggen
+            </button>
+        </div>
+    </div>
+  );
+};
+
+export default Apl_Header;
