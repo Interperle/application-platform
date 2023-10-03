e3742de3c6a51c547e069b2c0cdddd8f184ba77a
Marib Aldoais <marib.aldoais@gmail.com>
2023-10-03 11:46:44 +0200

    Created Overview Page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,113 +1,62 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="grid-cols-1 flex flex-col items-start justify-between p-24 max-w-5xl bg-[#FFFFFF] text-[#153757] space-y-4">
+        <div className='flex flex-col items-start justify-between space-y-4'>
+          <h1 className="text-lg font-bold">
+            Herzlich willkommen zur Generation-D Bewerbung 2023!
+          </h1>
+          <p>
+            Wir freuen uns sehr, dass Ihr Euch bei Generation-D bewerben möchtet.
+          </p>
+          <p>
+            Aktuell läuft die erste Bewerbungsphase, in welcher es darum geht einige Fragen zu Euerem Start-up/Projekt zu beantworten und zu jedem Teammitglied einen Lebenslauf hochzuladen.
+          </p>
+          <strong>
+            Seid Euch bewusst, dass Generation-D keine Bewerbungen akzeptieren kann…
+          </strong>
+          <ul className="list-none pl-5">
+            <li>
+              …in welchen ein Teammitglied aktueller oder ehemaliger Stipendiat*in der Bayerischen EliteAkademie ist, 
+            </li>
+            <li>
+              …das Projekt bereits in einem Generation-D Finale gewesen ist,
+            </li>
+            <li>
+              …es sich um ein langfristig rein spenden basiertes Projekt handelt,
+            </li>
+            <li>
+              …oder das Unternehmen bereits länger als 5 Jahre im Handelsregister eingetragen ist.
+            </li>
+          </ul>
+          <p>
+            Weitere Details zum Ablauf und der Bewertung könnt Ihr auch nochmal in unserem <Link href="https://generation-d.org/wp-content/uploads/2022/12/Ablauf_des_Bewerbungsprozesses_2023.pdf" target='_blank' className="font-bold font-underline">Leitfaden</Link> nachlesen.
+          </p>
+          
+          <h2 className="font-bold">Kurz noch einige allgemeine Hinweise:</h2>
+          <p>
+            Alle eure Angaben werden automatisch gespeichert. Ihr könnt eure Antworten aber vor der Deadline (<strong>18.02.2023</strong>) so oft Ihr wollt weiterhin bearbeiten. 
+          </p>
+          <p>
+            Eine Bewerbung wird erst berücksichtigt, wenn sie vollständig ist. Vollständig ist eine Bewerbung, wenn alle Fragen beantwortet wurden. Den Status eurer Bewerbung könnt Ihr auch immer auf dieser Übersichtsseite einsehen.
+          </p>
+          <p>
+            <i>Ihr werdet auch über alle weiteren Phasen und Vorgehen per E-Mail informiert.</i> Kontrolliert dafür bitte, dass die angegebene E-Mail korrekt ist und checkt auch den Spam Ordner.
+          </p>
+          <h3 className='font-bold'>Dann kann es jetzt losgehen! Klickt unten auf “Bewerbung bearbeiten” und startet durch!</h3>
+          <p>Wir freuen uns auf Euch und eure Ideen und wünschen Euch viel Erfolg!</p>
+          <p>
+            Euer Generation-D Team
+          </p>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <div className="w-full max-w-screen-xl mx-auto bg-white p-8 rounded-lg shadow-md">
+            <div className="grid grid-cols-3 gap-4">
+                <h2 className="p-4 rounded font-bold">Bewerbungs-Phase 1</h2>
+                <div className="p-4 rounded">PHASEN_STATUS_HIER</div>
+                <button className="border rounded px-4 py-2 text-[#FDCC89] bg-[#153757]">Phase fortsetzen</button>
+            </div>
+        </div>
     </main>
   )
 }
