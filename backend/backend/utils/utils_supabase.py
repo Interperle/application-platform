71a9e13ea8d5b27f1d662acc259c6224c77e6fa0
Marib Aldoais <marib.aldoais@gmail.com>
2023-10-19 01:15:12 +0200

    init supabase with and without rls

---
+++ backend/backend/utils/utils_supabase.py
@@ -0,0 +1,12 @@
+import os
+
+
+from supabase import create_client
+
+def init_supabase():
+    # use SUPABASE_SERVICE_KEY to ignore Row Level Security
+    return create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_SERVICE_KEY'))
+
+def init_supabase_with_rls():
+    # use SUPABASE_ANON_KEY to accept Row Level Security
+    return create_client(os.getenv('SUPABASE_URL'), os.getenv('SUPABASE_ANON_KEY'))
