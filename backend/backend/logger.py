025a06fc137d37291be3b8398618cdc0a62a4217
Marib Aldoais <marib.aldoais@gmail.com>
2023-10-19 01:11:32 +0200

    added timeout and fixed error handling

--- backend/backend/logger.py
+++ backend/backend/logger.py
@@ -37,13 +37,12 @@ class Logger:
             }
         }
         try:
-            
-            response = requests.post(self.api_url, headers=self.HEADER, json=payload)
+            response = requests.post(self.api_url, headers=self.HEADER, json=payload, timeout=5.0)
             if response.status_code != 200:
-                self.logger.error("Failed to send log to API. Status code:", response.status_code)
+                self.logger.error("Failed to send log to API. Status code: %s", response.status_code)
         except requests.RequestException as e:
-            self.logger.error("Failed to send log to API. Error:", str(e))
-    
+            self.logger.error("Failed to send log to API. Error: %s", str(e))
+
 
 def run():
     log = Logger("TEST")
