90d7b4e55b476645a7d2dcdfadcb6ddadc6f9b92
Marib Aldoais <marib.aldoais@gmail.com>
2023-10-19 01:14:54 +0200

    refactoring

---
+++ backend/backend/utils/utils_file.py
@@ -0,0 +1,15 @@
+import yaml
+
+def read_yaml_file(filename):
+    """
+    Read the content of a YAML file and return the data.
+
+    :param filename: str, the name (and path) of the YAML file.
+    :return: dict, the data from the YAML file.
+    """
+    with open(filename, 'r') as stream:
+        try:
+            return yaml.safe_load(stream)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
