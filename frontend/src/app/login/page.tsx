8aedea6100320d5df309cfaa96b4020869dc8197
Marib Aldoais <marib.aldoais@gmail.com>
2023-10-20 22:33:22 +0200

    using popup component

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,15 +1,13 @@
 "use client"
 import Image from 'next/image'
 import SignInForm from '@/utils/forms/signin-form'
-import SignUpPopup from '@/components/signupPopup'
-import PasswordResetForm from '@/utils/forms/passwortreset-form'
 import SignUpForm from '@/utils/forms/signup-form'
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { SetStateAction, useState } from 'react'
+import { openPopup } from '@/store/slices/popupSlice';
+import Popup from '@/components/popup'
+import { useAppDispatch } from '@/store/store'
 
 export default function Login() {
-  const [showSignUpPopup, setShowSignUpPopup] = useState(false);
-
+  const dispatch = useAppDispatch();
   return (
     <main className="grid-cols-1 flex flex-col items-center justify-between space-y-6">
       <Image src="/logos/gend_img.png" width={80} height={80} alt="Generation-D Image Logo" className="max-w-50 max-h-50" />
@@ -20,11 +18,13 @@ export default function Login() {
         <p>
           Um eine neue Bewerbung zu starten, lege ein neues Konto an. Wenn du bereits ein Konto hast, logge dich ein um deine Bewerbung fortzusetzen.
         </p>
-        <button type="button" onClick={() => setShowSignUpPopup(true)} className="border rounded px-4 py-2 text-[#FDCC89] bg-[#153757]">
+        <button type="button" onClick={() => dispatch(openPopup())} className="border rounded px-4 py-2 text-[#FDCC89] bg-[#153757]">
           Neues Konto anlegen
         </button>
         <div  className='text-left'>
-          {showSignUpPopup && <SignUpPopup onClose={() => setShowSignUpPopup(false)} isOpen={true} />}
+          <Popup>
+            <SignUpForm/>
+          </Popup>
         </div>
         <p>
           Oder logge dich ein, um deine Bewerbung fortzusetzen
