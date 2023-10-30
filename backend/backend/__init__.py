73b0d92c07bf489a8b0f5756fa33c0a95cc54b82
luca <Scalerandi.Luca@gmail.com>
2023-10-30 15:48:03 +0100

    logger updated and tests added

--- backend/backend/__init__.py
+++ backend/backend/__init__.py
@@ -1,3 +1,3 @@
 from dotenv import load_dotenv
 
-load_dotenv('.env.local')
+load_dotenv('backend/.env.local')
--- backend/backend/logger.py
+++ backend/backend/logger.py
@@ -6,26 +6,27 @@ import requests
 class Logger:
     def __init__(self, module: str):
         self.api_url = f"https://api.logflare.app/api/logs?source={os.environ['LOGFLARE_BACKEND_SOURCE_TOKEN']}"
+        self.api_get_url = f"https://api.logflare.app/api/endpoints/query/{os.environ['LOGFLARE_BACKEND_SOURCE_TOKEN']}"
         self.HEADER = {'Content-Type': 'application/json', 'x-api-key': os.environ['LOGFLARE_BACKEND_API_TOKEN']}
         self.logger = logging.getLogger('ApiLogger')
         self.module = module
 
-    def debug(self, message):
-        self.logger.debug(message, "debug")
+    def debug(self, message: str) -> None:
+        return self.logger.debug(message)
 
-    def info(self, message):
-        self.__send_log_to_log_flare(message, "info")
+    def info(self, message: str) -> bool:
+        return self.__send_log_to_log_flare(message, "info")
 
-    def warning(self, message):
-        self.__send_log_to_log_flare(message, "warning")
+    def warning(self, message: str) -> bool:
+        return self.__send_log_to_log_flare(message, "warning")
 
-    def error(self, message):
-        self.__send_log_to_log_flare(message, "error")
+    def error(self, message: str) -> bool:
+        return self.__send_log_to_log_flare(message, "error")
 
-    def fatal(self, message):
-        self.__send_log_to_log_flare(message, "fatal")
+    def fatal(self, message: str) -> bool:
+        return self.__send_log_to_log_flare(message, "fatal")
 
-    def __send_log_to_log_flare(self, message, level):
+    def __send_log_to_log_flare(self, message: str, level: int) -> bool:
         payload = {
             "event_message": message,
             "metadata": {
@@ -37,8 +38,11 @@ class Logger:
             response = requests.post(self.api_url, headers=self.HEADER, json=payload, timeout=5.0)
             if response.status_code != 200:
                 self.logger.error("Failed to send log to API. Status code: %s", response.status_code)
+                assert False
         except requests.RequestException as e:
             self.logger.error("Failed to send log to API. Error: %s", str(e))
+            return False
+        return True
 
 
 def run():
