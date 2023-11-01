873c56eb6a5be17d17de41c97f9402ff2bdef8e2
Marib Aldoais <marib.aldoais@gmail.com>
2023-11-01 20:57:45 +0100

    finished process_config for new questiontypes

--- backend/backend/process_config.py
+++ backend/backend/process_config.py
@@ -4,7 +4,7 @@ from backend.utils.consts import DATETIME_FORMAT
 from backend.utils.utils_file import read_yaml_file
 from backend.utils.utils_supabase import init_supabase
 from backend.enums.question_type import QuestionType
-from backend.validate_config import run_structure_checks
+from backend.validate_config import ADDITIONAL_PARAMS, QUESTION_TYPES_DB_TABLE, run_structure_checks
 
 log = Logger(__name__)
 
@@ -25,33 +25,33 @@ def process_config():
         for question in phase["questions"]:
             question_type = QuestionType.str_to_enum(question['questionType'])
             data_question_table = create_data_questions_table(question_type, question["order"], phase_id,
-                                                              question["mandatory"])
+                                                              question["mandatory"], question["question"])
             log.info(f"Create Question '{question}'")
             response_question_table = supabase.table("question_table").insert(data_question_table).execute()
             log.info(str(response_question_table))
             log.info(f"Create QuestionType {question_type}")
             question_id = response_question_table.data[0]["questionid"]
-            data_question_type_table = create_data_question_types_table(question_id, question["question"])
-            if question_type == QuestionType.SHORT_TEXT:
-                response_question_type_table = supabase.table("short_text_question_table").insert(
-                    data_question_type_table).execute()
-            elif question_type == QuestionType.LONG_TEXT:
-                response_question_type_table = supabase.table("long_text_question_table").insert(
-                    data_question_type_table).execute()
-            elif question_type == QuestionType.MULTIPLE_CHOICE:
-                data_question_type_table["numberofpossibleanswers"] = question["numberOfPossibleAnswers"]
-                response_question_type_table = supabase.table("multiple_choice_question_table").insert(
-                    data_question_type_table).execute()
-                for answer in question["Answers"]:
-                    data_choice_table = create_data_choice_table(question_id, answer)
-                    response_question_type_table = supabase.table("multiple_choice_question_choices_table").insert(
-                        data_choice_table).execute()
-            elif question_type == QuestionType.VIDEO_UPLOAD:
-                response_question_type_table = supabase.table("video_question_table").insert(
-                    data_question_type_table).execute()
-            else:
-                raise ValueError("No other QuestionTypes defined!")
+            data_question_type_table = create_data_question_types_table(question_id)
+            param: str
+            for param in ADDITIONAL_PARAMS.get(question_type, {}):
+                if param != "Answers":
+                    data_question_type_table[param.lower()] = str(question[param])
+            response_question_type_table = supabase.table(QUESTION_TYPES_DB_TABLE[question_type]).insert(data_question_type_table).execute()
             log.info(str(response_question_type_table))
+            if question_type == QuestionType.MULTIPLE_CHOICE:
+                for answer in question["Answers"]:
+                    data_list_table = create_data_choice_table(question_id, answer)
+                    table_name = "multiple_choice_question_choice_table"
+                    response_list_table = supabase.table(table_name).insert(
+                        data_list_table).execute()
+                    log.info(str(response_list_table))
+            elif question_type == QuestionType.DROPDOWN:
+                for answer in question["Answers"]:
+                    data_list_table = create_data_option_table(question_id, answer)
+                    table_name = "dropdown_question_option_table"
+                    response_list_table = supabase.table(table_name).insert(
+                        data_list_table).execute()
+                    log.info(str(response_list_table))
         log.info(f"Processed Phase {phase} successfully")
 
 
@@ -64,19 +64,19 @@ def create_data_phase_table(phasename: str, ordernumber: int, startdate: datetim
     }
 
 
-def create_data_questions_table(questiontype: QuestionType, ordernumber: int, phaseid: str, mandatory: bool):
+def create_data_questions_table(questiontype: QuestionType, ordernumber: int, phaseid: str, mandatory: bool, question: str):
     return {
         "questiontype": str(questiontype),
         "questionorder": ordernumber,
         "phaseid": phaseid,
         "mandatory": 1 if mandatory else 0,
+        "questiontext": question,
     }
 
 
-def create_data_question_types_table(question_id: str, question: str):
+def create_data_question_types_table(question_id: str):
     return {
         "questionid": question_id,
-        "questiontext": question,
     }
 
 
@@ -84,4 +84,11 @@ def create_data_choice_table(questionId: str, choiceText: str):
     return {
         "questionid": questionId,
         "choicetext": choiceText,
+    }
+
+
+def create_data_option_table(questionId: str, optionText: str):
+    return {
+        "questionid": questionId,
+        "optiontext": optionText,
     }
