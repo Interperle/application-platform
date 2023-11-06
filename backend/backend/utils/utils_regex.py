8eff2339c861212e79c36ef3a209296461dfaec3
Marib Aldoais <marib.aldoais@gmail.com>
2023-11-06 18:18:50 +0100

    renamed file and refactored

--- backend/backend/utils/utils_regex.py
+++ backend/backend/utils/utils_regex.py
@@ -1,14 +1,5 @@
 import re
-
-REGEX = {
-    'PHONE_NUMBER': r'^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$',
-    'URL_SEGMENT': r"^(_|-|[a-z]|[A-Z]|[0-9])+$",
-    'EMAIL': r"^[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+)*@" + \
-                            r"(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$",
-    'TEXT': r'^[a-zA-Z0-9_ ]*$',
-    'NATURAL_NUMBER':  r'^[1-9]\d+$',
-    'RATIONAL_NUMBER': r'^(-?)(0|[1-9]\d*)([.](\d*)[1-9])?$'
-}
+from consts import REGEX_PY
 
 
 def phone_number_regex_check(string: str) -> bool:
@@ -16,7 +7,7 @@ def phone_number_regex_check(string: str) -> bool:
     Simple phone number regex check.
     See: https://uibakery.io/regex-library/phone-number-python
     '''
-    return bool(re.match(REGEX['PHONE_NUMBER'], string))
+    return bool(re.match(REGEX_PY['PHONE_NUMBER'], string))
 
 
 def email_regex_check(string: str) -> bool:
@@ -24,7 +15,7 @@ def email_regex_check(string: str) -> bool:
     Simple email regex check.
     See: https://uibakery.io/regex-library/email-regex-python
     '''
-    return bool(re.match(REGEX['EMAIL'], string))
+    return bool(re.match(REGEX_PY['EMAIL'], string))
 
 
 def natural_number_check(string: str) -> bool:
@@ -32,7 +23,7 @@ def natural_number_check(string: str) -> bool:
     Simple check if string contains a natural number (>=0). Leading '+' is not allowed.
     NOTE: if superscripts like ² and fraction value like ½ are allowed, use python's isdigit() instead.
     '''
-    return bool(re.match(REGEX['NATURAL_NUMBER'], string))
+    return bool(re.match(REGEX_PY['NATURAL_NUMBER'], string))
 
 
 def rational_number_check(string: str) -> bool:
@@ -40,11 +31,11 @@ def rational_number_check(string: str) -> bool:
     Simple check if string contains a rational number. Leading '+' is not allowed.
     NOTE: if superscripts like ² and fraction value like ½ are allowed, use python's isnumeric() instead.
     '''
-    return bool(re.match(REGEX['RATIONAL_NUMBER'], string))
+    return bool(re.match(REGEX_PY['RATIONAL_NUMBER'], string))
 
 
 def url_phase_segment_check(string: str) -> bool:
     '''
     Simple check if string contains a valid phase segment for the URL.
     '''
-    return bool(re.match(REGEX['URL_SEGMENT'], string))
+    return bool(re.match(REGEX_PY['URL_SEGMENT'], string))
