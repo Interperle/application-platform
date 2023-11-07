937d381f477f4e9a57735cb0fa0c85ab007f87d9
Marib Aldoais <marib.aldoais@gmail.com>
2023-11-07 23:36:50 +0100

    importing supabase from ssr

--- frontend/src/app/auth/callback/route.ts
+++ frontend/src/app/auth/callback/route.ts
@@ -1,25 +1,13 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { supabase } from "@/utils/supabase_route";
+import { NextResponse } from "next/server";
 
-import type { NextRequest } from 'next/server'
-import type { Database } from '@/types/supabase'
-
-export async function GET(request: NextRequest) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+export async function GET(request: Request) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    const cookieStore = cookies()
-    const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore })
-    await supabase.auth.exchangeCodeForSession(code)
+    await supabase.auth.exchangeCodeForSession(code);
   }
 
-  const next = requestUrl.searchParams.get('next')
-  if (next) {
-    return NextResponse.redirect(next)
-  }
-
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin)
+  return NextResponse.redirect(requestUrl.origin);
 }
