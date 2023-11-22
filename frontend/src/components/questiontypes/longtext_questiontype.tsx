17e908008c280968e0fedf8a96c0a2dec4a76006
Marib Aldoais <marib.aldoais@gmail.com>
2023-11-22 23:00:43 +0100

    prettified input boxes

--- frontend/src/components/questiontypes/longtext_questiontype.tsx
+++ frontend/src/components/questiontypes/longtext_questiontype.tsx
@@ -17,7 +17,7 @@ const LongTextQuestionType: React.FC<LongTextQuestionTypeProps> = ({
       questionnote={questionnote}
     >
       <textarea
-        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md resize-none"
+        className="shadow appearance-none border rounded-md w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-primary focus:border-primary transition duration-150 ease-in-out resize-none"
         required={mandatory}
         maxLength={200}
         rows={4}
--- frontend/src/components/questiontypes/shorttext_questiontype.tsx
+++ frontend/src/components/questiontypes/shorttext_questiontype.tsx
@@ -20,7 +20,7 @@ const ShortTextQuestionType: React.FC<ShortTextQuestionTypeProps> = ({
         type="text"
         name={questionid}
         id={questionid}
-        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+        className="shadow appearance-none border rounded-md w-full py-2 px-3 text-secondary leading-tight focus:outline-none focus:shadow-outline focus:ring-2 focus:ring-primary focus:border-primary transition duration-150 ease-in-out"
         required={mandatory}
         maxLength={50}
       />
