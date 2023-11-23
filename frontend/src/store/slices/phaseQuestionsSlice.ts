f58a094e07ef24a38da32072749860dbe22c19a6
Marib Aldoais <marib.aldoais@gmail.com>
2023-11-23 23:47:01 +0100

    set phaseQuestionsSlice

--- frontend/src/store/slices/phaseQuestionsSlice.ts
+++ frontend/src/store/slices/phaseQuestionsSlice.ts
@@ -1,33 +1,53 @@
 import { Question } from "@/components/questions";
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-
-interface PhasesState {
-    phases: {
-      [phasename: string]: Question[];
-    };
+interface PhaseData {
+  phaseid: string,
+  phasename: string,
+  phaseorder: number,
+  startdate: string,
+  enddate: string
 }
 
-// Define the payload structure for the action
-interface SetPhaseQuestionsPayload {
-    phasename: string;
-    phasequestions: Question[];
+interface PhasesState {
+  phases: {
+    [phasename: string]: {
+      data: PhaseData;
+      questions: Question[];
+    };
+  };
 }
 
 const initialState: PhasesState = {
-    phases: {},
+  phases: {},
 };
 
 export const phaseSlice = createSlice({
   name: "phase",
-  initialState: initialState,
+  initialState,
   reducers: {
-    setPhaseQuestions: (state, action: PayloadAction<SetPhaseQuestionsPayload>) => {
-        const { phasename, phasequestions } = action.payload;
-        state.phases[phasename] = phasequestions;
+    setPhaseData: (
+      state,
+      action: PayloadAction<{ phasename: string; phasedata: PhaseData }>,
+    ) => {
+      const { phasename, phasedata } = action.payload;
+      if (!state.phases[phasename]) {
+        state.phases[phasename] = { data: {phaseid: "", phasename:"", phaseorder: -1, startdate: "", enddate: ""}, questions: []};
+      }
+      state.phases[phasename].data = phasedata;
+    },
+    setPhaseQuestions: (
+      state,
+      action: PayloadAction<{ phasename: string; phasequestions: Question[] }>,
+    ) => {
+      const { phasename, phasequestions } = action.payload;
+      if (!state.phases[phasename]) {
+        state.phases[phasename] = { data: {phaseid: "", phasename:"", phaseorder: -1, startdate: "", enddate: ""}, questions: [] };
+      }
+      state.phases[phasename].questions = phasequestions;
     },
   },
 });
 
-export const { setPhaseQuestions } = phaseSlice.actions;
+export const { setPhaseQuestions, setPhaseData } = phaseSlice.actions;
 export default phaseSlice.reducer;
