1887154fae132f27d4e7d6d2ca08120dee440703
Marib Aldoais <marib.aldoais@gmail.com>
2023-11-26 17:05:03 +0100

    refactored redux

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,23 +1,32 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
+import {
+  TypedUseSelectorHook,
+  useSelector,
+  useDispatch,
+  useStore,
+} from "react-redux";
 import authReducer from "./slices/authSlice";
 import popupReducer from "./slices/popupSlice";
 import userReducer from "./slices/usersSlice";
 import menuReducer from "./slices/menuSlice";
-import phaseQuestionsReducer from "./slices/phaseQuestionsSlice";
+import phaseReducer from "./slices/phaseSlice";
 
-export const store = configureStore({
-  reducer: {
-    authReducer,
-    popupReducer,
-    userReducer,
-    menuReducer,
-    phaseQuestionsReducer,
-  },
-});
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      authReducer,
+      popupReducer,
+      userReducer,
+      menuReducer,
+      phaseReducer,
+    },
+  });
+};
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore: () => AppStore = useStore;
