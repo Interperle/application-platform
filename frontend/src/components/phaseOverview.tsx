b1cc1e16bc4fa0a5eec57e514ae0f42b5911b921
Marib Aldoais <marib.aldoais@gmail.com>
2023-11-29 00:43:37 +0100

    added overview over phases

---
+++ frontend/src/components/phaseOverview.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { transformReadableDate } from "@/utils/helpers";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { ProgressBar } from "./progressbar";
+
+
+const PhaseOverview: React.FC<{key: string, phaseName: string, phaseStart: string, phaseEnd: string, mandatoryQuestionIds: string[], numAnswers: number}> = ({key, phaseName, phaseStart, phaseEnd, mandatoryQuestionIds, numAnswers}) => {
+    const router = useRouter();
+    const handleRedirect = () => {
+        router.push(`/${phaseName}`);
+    };
+
+    return (
+        <div key={key} className="w-full max-w-screen-xl mx-auto bg-white p-8 rounded-lg shadow-md">
+            <div className="grid grid-cols-3 gap-4">
+                <div className="grid grid-rows-2">
+                    <h2 className="rounded font-bold">{phaseName}</h2>
+                    <h4 className="rounded">{transformReadableDate(phaseStart)} - {transformReadableDate(phaseEnd)}</h4>
+                </div>
+                <div className="p-4 rounded"><ProgressBar mandatoryQuestionIds={mandatoryQuestionIds} numAnswers={numAnswers}/></div>
+                <button onClick={() => handleRedirect()} className="apl-button-fixed-short">Phase fortsetzen</button>
+            </div>
+        </div>
+    );
+};
+
+export default PhaseOverview;
+
+
+
