864cbfcf8be1a5bb7c83ae2fd2a742c6cc46da01
Marib Aldoais <marib.aldoais@gmail.com>
2023-12-05 22:51:36 +0100

    Added minanswers and maxanswers

--- frontend/src/components/questiontypes/dropdown_questiontype.tsx
+++ frontend/src/components/questiontypes/dropdown_questiontype.tsx
@@ -11,6 +11,9 @@ import { AwaitingChild } from "../awaiting";
 export interface DropdownQuestionTypeProps extends DefaultQuestionTypeProps {
   answerid: string | null;
   options: DropdownOptionProps[];
+  minanswers: number;
+  maxanswers: number;
+  userinput: boolean;
 }
 
 const DropdownQuestionType: React.FC<DropdownQuestionTypeProps> = ({
@@ -20,10 +23,14 @@ const DropdownQuestionType: React.FC<DropdownQuestionTypeProps> = ({
   questiontext,
   questionnote,
   questionorder,
+  minanswers,
+  maxanswers,
+  userinput,
   answerid,
   options,
 }) => {
-  const [answer, setAnswer] = useState("");
+  const [singleAnswer, setSingleAnswer] = useState("");
+  const [multiAnswer, setMultiAnswer] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -31,22 +38,39 @@ const DropdownQuestionType: React.FC<DropdownQuestionTypeProps> = ({
       try {
         if (answerid) {
           const savedAnswer = await fetchDropdownAnswer(answerid);
-          setAnswer(savedAnswer || "");
+          if (maxanswers == 1){
+            setSingleAnswer(savedAnswer || "");
+          } else {
+            setMultiAnswer(savedAnswer.split(",") || [])
+          }
         }
         setIsLoading(false);
       } catch (error) {
-        console.error("Failed to fetch answer", error);
+        console.error("Failed to fetch singleAnswer", error);
       }
     }
     loadAnswer();
   }, [questionid, answerid]);
 
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  const handleSingleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedOption =
       event.target.options[event.target.selectedIndex].text;
     saveDropdownAnswer(selectedOption, questionid);
-    setAnswer(event.target.value);
+    setSingleAnswer(event.target.value);
   };
+
+  const handleMultiChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedOptions = Array.from(event.target.selectedOptions, (option) => option.value).filter((value) => value !== "empty");
+    if ((!mandatory || (selectedOptions.length >= minanswers) && mandatory) && (selectedOptions.length <= maxanswers)) {
+      saveDropdownAnswer(selectedOptions.toString(), questionid);
+      setMultiAnswer(selectedOptions);
+    } else {
+      setMultiAnswer(selectedOptions);
+      alert("Du musst mindestens " + minanswers + " und kannst maximal " + maxanswers + " Antworten auswählen!")
+    }
+    
+  };
+
   return (
     <QuestionTypes
       phasename={phasename}
@@ -57,28 +81,53 @@ const DropdownQuestionType: React.FC<DropdownQuestionTypeProps> = ({
       questionorder={questionorder}
     >
       <AwaitingChild isLoading={isLoading}>
-        <select
-          id={questionid}
-          name={questionid}
-          required={mandatory}
-          className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          onChange={handleChange}
-          value={answer}
-        >
-          {answer === "" && (
-            <option key="invalid" value="" disabled hidden>
-              Bitte wähle eine Option
-            </option>
-          )}
-          {!mandatory && <option key="empty" value="empty"></option>}
-          {options.map((option) => (
-            <DropdownOption
-              key={option.optionid}
-              optionid={option.optionid}
-              optiontext={option.optiontext}
-            />
-          ))}
-        </select>
+        {(maxanswers == 1) ? (
+          <select
+            id={questionid}
+            name={questionid}
+            required={mandatory}
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            onChange={handleSingleChange}
+            value={singleAnswer}
+          >
+            {singleAnswer === "" && (
+              <option key="invalid" value="" disabled hidden>
+                Bitte wähle eine Option
+              </option>
+            )}
+            {!mandatory && <option key="empty" value="empty"></option>}
+            {options.map((option) => (
+              <DropdownOption
+                key={option.optionid}
+                optionid={option.optionid}
+                optiontext={option.optiontext}
+              />
+            ))}
+          </select>) : (
+          <>
+            <span className="italic text-gray-500 text-sm">Um mehrere Optionen auszuwählen, bitte halte unter Windows die "Alt" und unter Mac die "CMD" Taste gedrückt.</span>
+            <select
+              multiple
+              size={maxanswers}
+              id={questionid}
+              name={questionid}
+              required={mandatory}
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              onChange={handleMultiChange}
+              value={multiAnswer}
+            >
+              {!mandatory && <option key="empty" value="empty"></option>}
+              {options.map((option) => (
+                <DropdownOption
+                  key={option.optionid}
+                  optionid={option.optionid}
+                  optiontext={option.optiontext}
+                />
+              ))}
+            </select>
+          </>
+        )
+        }
       </AwaitingChild>
     </QuestionTypes>
   );
