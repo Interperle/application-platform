5fbea24cde2b649809a622b03daacb98bf6c67f3
Marib Aldoais <marib.aldoais@gmail.com>
2023-12-27 23:42:47 +0100

    fixed Admin access

--- frontend/src/app/auth/callback/route.ts
+++ frontend/src/app/auth/callback/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 import { getURL } from "@/utils/helpers";
 import {
-  initSupabaseRoute,
+  initSupabaseActions,
   supabaseServiceRole,
 } from "@/utils/supabaseServerClients";
 
@@ -10,14 +10,16 @@ export async function GET(request: Request) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
-  const supabase = initSupabaseRoute();
+  const supabase = initSupabaseActions();
 
   if (code) {
+    console.log("Code: " + code)
     const data = await supabase.auth.exchangeCodeForSession(code);
   }
   const {
     data: { user },
   } = await supabase.auth.getUser();
+  console.log("User: " + JSON.stringify(user))
 
   const { data: roleData, error: roleError } = await supabase
     .from("user_profiles_table")
