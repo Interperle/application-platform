c6c7d4d32fd8941ca2a1a9766867e3d90d939c30
Marib Aldoais <marib.aldoais@gmail.com>
2023-12-27 18:08:14 +0100

    added easteregg

--- frontend/src/app/review/page.tsx
+++ frontend/src/app/review/page.tsx
@@ -1,35 +1,10 @@
-"use client"
-import { downloadFile } from "@/utils/helpers";
-import Image from "next/image";
-import { useEffect, useState } from "react";
-
+import Easteregg from "@/components/easteregg";
 
 export default function Page() {
-  const [easteregg, setEasteregg] = useState("");
-
-  useEffect(() => {
-    async function loadAnswer() {
-      const imageUploadBucketData = await downloadFile(
-        "eastereggs",
-        "marib.jpg",
-      );
-      const url = URL.createObjectURL(imageUploadBucketData!);
-      setEasteregg(url || "");
-    }
-    loadAnswer();
-  });
-
   return (
     <div>
       <div>REVIEW DASHBOARD</div>
-      <Image
-        alt="Preview"
-        src={easteregg}
-        className="self-center max-w-xs max-h-96"
-        id="imagePreview"
-        width={100}
-        height={100}
-      />
+      <Easteregg person="maxundchris" />
       <div>Under Construction</div>
     </div>
   );
