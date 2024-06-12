ccb63fa1c650805d8d268c340a6a67671c32f4f1
Interperle <marib.aldoais@googlemail.com>
2024-06-12 18:21:24 +0200

    fixed paths

--- frontend/src/utils/getMarkdownText.ts
+++ frontend/src/utils/getMarkdownText.ts
@@ -15,8 +15,9 @@ export default async function getOverviewPageText() {
   phases_status.sort((a, b) => b.phase.phaseorder - a.phase.phaseorder);
   const last_phase_status = phases_status[0];
   let markdownFilePath: string;
+  const base_branch = path.join(process.cwd(), "public", "texts")
   if (currentPhase.phaseorder == -1) {
-    markdownFilePath = path.join("..", "..", "public", "texts", "welcome.md");
+    markdownFilePath = path.join(base_branch, "welcome.md");
   } else if (
     last_phase_status !== undefined &&
     last_phase_status.outcome == false &&
@@ -24,51 +25,36 @@ export default async function getOverviewPageText() {
     currentPhase.phaseid != last_phase_status.phase.phaseid
   ) {
     markdownFilePath = path.join(
-      "..",
-      "..",
-      "public",
-      "texts",
+      base_branch,
       last_phase_status.phase.phasename,
       "failed.md",
     );
   } else if (new Date(currentPhase.enddate) >= currentTime) {
     markdownFilePath = path.join(
-      "..",
-      "..",
-      "public",
-      "texts",
+      base_branch,
       currentPhase.phasename,
       "ongoing.md",
     );
   } else if (currentPhase.finished_evaluation == null) {
     markdownFilePath = path.join(
-      "..",
-      "..",
-      "public",
-      "texts",
+      base_branch,
       currentPhase.phasename,
       "evaluating.md",
     );
   } else if (last_phase_status.outcome == false) {
     markdownFilePath = path.join(
-      "..",
-      "..",
-      "public",
-      "texts",
+      base_branch,
       currentPhase.phasename,
       "failed.md",
     );
   } else if (last_phase_status.outcome == true) {
     markdownFilePath = path.join(
-      "..",
-      "..",
-      "public",
-      "texts",
+      base_branch,
       currentPhase.phasename,
       "passed.md",
     );
   } else {
-    markdownFilePath = path.join("..", "..", "public", "texts", "error.md");
+    markdownFilePath = path.join(base_branch, "error.md");
   }
   const markdownContent = fs.readFileSync(markdownFilePath, "utf8");
   const contentHtml = await markdownToHtml(markdownContent);
